5562f19aa6c60823c77e275aff1e9941c164c7de
juliarta99 <adij4255@gmail.com>
2025-06-10 22:39:22 +0800

    feat: landingpage

--- js/nav.js
+++ js/nav.js
@@ -11,4 +11,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
     window.addEventListener("scroll", checkScroll);
     checkScroll();
+
+
+    // humberger
+    const hamburgers = document.querySelectorAll('.humberger');
+    const navMobile = document.querySelector('.nav-mobile');
+
+    hamburgers.forEach(hamburger => {
+        hamburger.addEventListener('click', () => {
+            hamburgers.forEach(item => item.classList.toggle('active'));
+            navMobile.classList.toggle('active');
+            document.body.classList.toggle('overflow-hidden');
+        });
+    });
 });
